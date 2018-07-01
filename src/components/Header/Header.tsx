dbee2701c5715bb6e8ba3dc33b5a375126cb5076
Piotr Kwiecinski <piokwiecinski@gmail.com>
2018-07-01 21:04:40 +0100

    Logo as separate component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react'
-import Link from 'gatsby-link'
 import styled from 'styled-components'
-import * as logo from '../../images/logo.svg'
+import Logo from '../Logo/Logo'
 
 interface HeaderProps {
   siteTitle: string
@@ -11,21 +10,12 @@ const StyledHeaderWrapper = styled.div`
   margin: 0 auto;
   max-width: 600px;
   padding: 1.45rem 1.0875rem;
-
-  a {
-    color: #000;
-    text-decoration: none;
-  }
 `
 
 const Header: React.SFC<HeaderProps> = ({ siteTitle }) => (
   <div className="content">
     <StyledHeaderWrapper>
-      <div>
-        <Link to="/" title="Back to homepage">
-          <img src={logo} alt={siteTitle} />
-        </Link>
-      </div>
+      <Logo alt={siteTitle}/>
     </StyledHeaderWrapper>
   </div>
 )
---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react'
+import Link from 'gatsby-link'
+import * as logo from './images/logo.svg'
+
+interface LogoProps {
+  alt?: string
+  src?: string
+}
+
+const Logo: React.SFC<LogoProps> = ({alt}) => (
+  <div>
+    <Link to="/" title="Back to homepage">
+      <img src={logo} alt={alt} />
+    </Link>
+  </div>
+)
+
+export default Logo
