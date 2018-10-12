530efa32e8177a69159322513690171c139adf5b
Karol <k.partyka87@gmail.com>
2018-10-12 23:22:18 +0200

    Changed quotes to backticks

--- src/enums/menuItems.js
+++ src/enums/menuItems.js
@@ -1,19 +1,19 @@
 const MENU_ITEMS = [
     {
-        name: 'Home',
-        targetElement: '123'
+        name: `Home`,
+        targetElement: `123`
     },
     {
-        name: 'About',
-        targetElement: '123'
+        name: `About`,
+        targetElement: `123`
     },
     {
-        name: 'Services',
-        targetElement: '321',
+        name: `Services`,
+        targetElement: `321`,
     },
     {
-        name: 'Contact',
-        targetElement: '456',
+        name: `Contact`,
+        targetElement: `456`,
     }
 ]
 
