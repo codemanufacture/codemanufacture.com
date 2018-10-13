b5cb1ab34d25aee499e93ba0c81065a1467d327b
Piotr Kwiecinski <piokwiecinski@gmail.com>
2018-10-13 20:00:04 +0100

    improvement: fix few codacy warnings for jest files (#14)

--- jest.config.js
+++ jest.config.js
@@ -1,46 +1,46 @@
 module.exports = {
-  "notify": true,
-  "verbose": true,
-  "transform": {
-    "^.+\\.tsx?$": "ts-jest",
-    "^.+\\.jsx?$": "<rootDir>/jest-transformer.js"
+  notify: true,
+  verbose: true,
+  transform: {
+    '^.+\\.tsx?$': `ts-jest`,
+    '^.+\\.jsx?$': `<rootDir>/jest-transformer.js`
   },
-  "testRegex": "\\.spec\\.(ts|tsx|js)$",
-  "moduleNameMapper": {
-    ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js"
+  testRegex: `\\.spec\\.(ts|tsx|js)$`,
+  moduleNameMapper: {
+    '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': `<rootDir>/__mocks__/fileMock.js`
   },
-  "testPathIgnorePatterns": [
-    "node_modules",
-    "/templates/",
-    ".cache"
+  testPathIgnorePatterns: [
+    `node_modules`,
+    `/templates/`,
+    `.cache`
   ],
-  "transformIgnorePatterns": [
-    "node_modules/(?!(gatsby)/)"
+  transformIgnorePatterns: [
+    `node_modules/(?!(gatsby)/)`
   ],
-  "globals": {
-    "__LINK_PREFIX__": "",
-    "__PATH_PREFIX__": ""
+  globals: {
+    "__LINK_PREFIX__": ``,
+    "__PATH_PREFIX__": ``
   },
-  "setupFiles": [
-    "<rootDir>/loadershim.js"
+  setupFiles: [
+    `<rootDir>/loadershim.js`
   ],
-  "collectCoverage": false,
-  "coverageReporters": [
-    "json-summary",
-    "text",
-    "html"
+  collectCoverage: false,
+  coverageReporters: [
+    `json-summary`,
+    `text`,
+    `html`
   ],
-  "moduleFileExtensions": [
-    "ts",
-    "tsx",
-    "js"
+  moduleFileExtensions: [
+    `ts`,
+    `tsx`,
+    `js`
   ],
-  "coverageThreshold": {
-    "global": {
-      "lines": 45,
-      "statements": 44,
-      "functions": 42,
-      "branches": 43
+  coverageThreshold: {
+    global: {
+      lines: 45,
+      statements: 44,
+      functions: 42,
+      branches: 43
     }
   }
 }
