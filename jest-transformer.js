735500696b7789cfd9066425a20c60a5fbb90ebf
Piotr Kwiecinski <piokwiecinski@gmail.com>
2018-10-13 19:56:44 +0100

    improvement: fix few codacy warnings for jest files

--- jest-transformer.js
+++ jest-transformer.js
@@ -1,8 +1,8 @@
 const babelOptions = {
-  presets: ["@babel/react", "@babel/env"],
+  presets: [`@babel/react`, `@babel/env`],
   plugins: [
-    "@babel/plugin-proposal-optional-chaining",
-    "@babel/plugin-proposal-class-properties",
+    `@babel/plugin-proposal-optional-chaining`,
+    `@babel/plugin-proposal-class-properties`,
   ],
 }
 
