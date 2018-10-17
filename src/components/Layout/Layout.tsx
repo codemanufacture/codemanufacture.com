4641d532b4a8c1054b739685cfa57222841cd3d4
Piotr Kwiecinski <piokwiecinski@gmail.com>
2018-10-17 19:35:26 +0100

    fix(deps): update styled-components

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react'
 import Header from '../Header'
+import GlobalStyle from '../../theme/globalStyle'
 
 const Layout: React.SFC = ({ children }) => (
   <React.Fragment>
     <Header siteTitle="Code Manufacture" />
+    <GlobalStyle />
     {children}
   </React.Fragment>
 )
