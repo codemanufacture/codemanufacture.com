7495ca65b914c8ed4c858e8f369ba4da42162f01
Karol <k.partyka87@gmail.com>
2018-11-10 18:43:18 +0100

    fix: header should be overlapped with footer
    Fixing header z-index to always be on top - on mobile it was overlaping the footer form.

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -3,5 +3,6 @@ import { transitions } from './transitions'
 import { displayDimensions } from './displayDimensions'
 import { sizes } from './sizes'
 import { typography } from './typography'
+import { zIndexes } from './zIndexes'
 
-export { colors, transitions, displayDimensions, sizes, typography }
+export { colors, transitions, displayDimensions, sizes, typography, zIndexes }
