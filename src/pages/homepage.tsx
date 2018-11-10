af94ca7417bb6a9defd2c62788f10d5dcfc36457
Karol <k.partyka87@gmail.com>
2018-11-10 20:05:38 +0100

    feat: navigation
    Fixed missing homepage sections

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -1,14 +1,25 @@
 import * as React from 'react'
 import Layout from '../components/Layout'
-import menuItems from '../enums/homepageSections'
 import HomepageSection from '../components/HomepageSection'
 import ServicesSection from '../components/ServicesSection'
+import MenuItemInterface from '../components/Menu/MenuItemInterface'
+
+const HOMEPAGE_SECTIONS: MenuItemInterface[] = [
+  {
+    name: `Home`,
+    sectionId: `home`,
+  },
+  {
+    name: `Services`,
+    sectionId: `services`,
+  },
+]
 
 const HomePageContentPlaceholder = () => <p>Lorem ipsum dolor sit amet</p>
 
 const HomePage = () => (
   <Layout>
-    {menuItems.map(item => (
+    {HOMEPAGE_SECTIONS.map(item => (
       <HomepageSection sectionId={item.sectionId}>
         {item.sectionId === 'services' ? (
           <ServicesSection />
