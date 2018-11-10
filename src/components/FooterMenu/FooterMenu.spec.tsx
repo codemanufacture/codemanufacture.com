f1a8710383a95d224adbad9e725b5f69b7aca819
Karol <k.partyka87@gmail.com>
2018-11-10 22:11:13 +0100

    feat: navigation
    Basic tests for menu components

---
+++ src/components/FooterMenu/FooterMenu.spec.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+import renderer from 'react-test-renderer'
+import FooterMenu from './FooterMenu'
+
+describe('FooterMenu component', () =>
+  it('renders correctly', () => {
+    const tree = renderer.create(<FooterMenu />).toJSON()
+    expect(tree).toMatchSnapshot()
+  }))
