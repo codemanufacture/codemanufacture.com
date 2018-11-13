3132c6aee78479942e4dcb20e349b4588fc128ba
Karol <k.partyka87@gmail.com>
2018-11-13 22:32:59 +0100

    feat: contact form
    Created ContactForm component with InputWrapper for it

--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -3,94 +3,93 @@ import styled from 'styled-components'
 import { colors } from '../../theme'
 import InputWrapper from './InputWrapper'
 
-const StyledContactForm = styled.div`
-`
+const StyledContactForm = styled.div``
 
 const formFields = {
-    name: {
-        label: 'Name',
-        id: 'name',
-        placeholder: 'Name',
-        type: 'text',
-    },
-    email: {
-        label: 'Email Address',
-        id: 'email',
-        placeholder: 'Email Address',
-        type: 'email'
-    },
-    phone: {
-        label: 'Phone',
-        id: 'phone',
-        placeholder: 'Phone',
-        type: 'phone'
-    },
-    subject: {
-        label: 'Subject',
-        id: 'subject',
-        placeholder: 'Subject',
-        type: 'text',
-    },
-    message: {
-        label: 'Your Message',
-        id: 'message',
-        placeholder: 'Your Message',
-        type: 'textarea'
-    },
+  name: {
+    label: 'Name',
+    id: 'name',
+    placeholder: 'Name',
+    type: 'text',
+  },
+  email: {
+    label: 'Email Address',
+    id: 'email',
+    placeholder: 'Email Address',
+    type: 'email',
+  },
+  phone: {
+    label: 'Phone',
+    id: 'phone',
+    placeholder: 'Phone',
+    type: 'phone',
+  },
+  subject: {
+    label: 'Subject',
+    id: 'subject',
+    placeholder: 'Subject',
+    type: 'text',
+  },
+  message: {
+    label: 'Your Message',
+    id: 'message',
+    placeholder: 'Your Message',
+    type: 'textarea',
+  },
 }
 
 interface ContactFormState {
-    values: object
-    errors: object
+  values: object
+  errors: object
 }
 
 interface ContactFormProps {
-    props: object
+  props: object
 }
 
-class ContactForm extends React.PureComponent<ContactFormProps, ContactFormState> {
-    constructor(props: object) {
-        super(props)
+class ContactForm extends React.PureComponent<
+  ContactFormProps,
+  ContactFormState
+> {
+  constructor(props: object) {
+    super(props)
 
-        this.state = {
-            values: {},
-            errors: {},
-        }
+    this.state = {
+      values: {},
+      errors: {},
     }
+  }
 
-    handleInputChange = (field: string, e: React.SyntheticEvent) => {
-        let values = this.state.values;
-        values[field] = e.target.value;
-        this.setState({ values });
-    }
+  handleInputChange = (field: string, e: React.SyntheticEvent) => {
+    let values = this.state.values
+    values[field] = e.target.value
+    this.setState({ values })
+  }
 
-    renderFormFields = (formFields: object) => {
-        const fieldNames = Object.keys(formFields);
+  renderFormFields = (formFields: object) => {
+    const fieldNames = Object.keys(formFields)
 
-        return fieldNames.map((field) => {
-            const fieldId = formFields[field].id
+    return fieldNames.map(field => {
+      const fieldId = formFields[field].id
 
-            return (
-                <InputWrapper
-                    value={this.state.values[fieldId]}
-                    onChange={(e) => this.handleInputChange(fieldId, e)}
-                    placeholder={formFields[field].placeholder}
-                    type={formFields[field].type}
-                    id={formFields[field].id}
-                    label={formFields[field].label}
-                />
-            )
-        })
-    }
+      return (
+        <InputWrapper
+          value={this.state.values[fieldId]}
+          onChange={e => this.handleInputChange(fieldId, e)}
+          placeholder={formFields[field].placeholder}
+          type={formFields[field].type}
+          id={formFields[field].id}
+          label={formFields[field].label}
+        />
+      )
+    })
+  }
 
-
-    render() {
-        return (
-            <StyledContactForm>
-                {this.renderFormFields(formFields)}
-            </StyledContactForm>
-        )
-    }
+  render() {
+    return (
+      <StyledContactForm>{this.renderFormFields(formFields)}</StyledContactForm>
+    )
+  }
 }
 
 export default ContactForm
--- src/components/ContactForm/InputWrapper.tsx
+++ src/components/ContactForm/InputWrapper.tsx
@@ -4,42 +4,55 @@ import { colors } from '../../theme'
 import classNames from 'classnames'
 
 const StyledInputWrapper = styled.fieldset`
-    &.withErrors {
-        input {
-            border-bottom-color: #E42C3E;
-        }
-    }
-
+  &.withErrors {
     input {
-        width: 100%;
-        height: 48px;
-        border: none;
-        border-bottom: 1px solid ${colors.borderColor};
-        outline: none;
+      border-bottom-color: #e42c3e;
     }
+  }
+
+  input {
+    width: 100%;
+    height: 48px;
+    border: none;
+    border-bottom: 1px solid ${colors.borderColor};
+    outline: none;
+  }
 `
 
-
 interface InputWrapperProps {
-    id: string
-    isInvalid: boolean
-    label: string
-    onChange(): void
-    placeholder: string
-    type: string
-    value: string
+  id: string
+  isInvalid: boolean
+  label: string
+  onChange(): void
+  placeholder: string
+  type: string
+  value: string
 }
 
-const InputWrapper: React.SFC<InputWrapperProps> = ({ onChange, value, placeholder, isInvalid, label, type, id }) => {
-    const wrapperClassName = classNames({
-        withErrors: isInvalid
-    })
+const InputWrapper: React.SFC<InputWrapperProps> = ({
+  onChange,
+  value,
+  placeholder,
+  isInvalid,
+  label,
+  type,
+  id,
+}) => {
+  const wrapperClassName = classNames({
+    withErrors: isInvalid,
+  })
 
-    return (
-        <StyledInputWrapper className={wrapperClassName}>
-            <input type={type} value={value} placeholder={placeholder} id={id} onChange={onChange}/>
-            <label htmlFor={id}>{label}</label>
-        </StyledInputWrapper>
-        )
-    }
+  return (
+    <StyledInputWrapper className={wrapperClassName}>
+      <input
+        type={type}
+        value={value}
+        placeholder={placeholder}
+        id={id}
+        onChange={onChange}
+      />
+      <label htmlFor={id}>{label}</label>
+    </StyledInputWrapper>
+  )
+}
 export default InputWrapper
