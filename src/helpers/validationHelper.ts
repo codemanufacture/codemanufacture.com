712bab3193a3b14c7305d6fe0b55856f4577d2f2
Karol <k.partyka87@gmail.com>
2018-12-01 13:08:55 +0100

    feat: contact form
    Updated types in validator helper
    Installed TS version of validator package

--- src/helpers/validationHelper.ts
+++ src/helpers/validationHelper.ts
@@ -1,6 +1,6 @@
 import validator from 'validator'
 
-export const validateText = text =>
+export const validateText = (text: string) =>
   text.length && validator.isLength(text, { min: 3 })
 
-export const validateEmail = email => validator.isEmail(email)
+export const validateEmail = (email: string) => validator.isEmail(email)
