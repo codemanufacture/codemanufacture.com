6fcdde0366ce0a250f253a8bfcfdfc9fdbc037ca
Karol <k.partyka87@gmail.com>
2018-12-21 10:26:33 +0100

    feat: blog post component
    Added simple blog post component to make PostTemplate template to pass data only

--- src/templates/PostTemplate.tsx
+++ src/templates/PostTemplate.tsx
@@ -1,47 +1,7 @@
 import * as React from 'react'
-import Helmet from 'react-helmet'
-import styled from 'styled-components'
-import Layout from '../components/Layout'
+import BlogPost from '../components/BlogPost'
 import { graphql } from 'gatsby'
-import { colors } from '../theme'
 import { Query } from '../graphql-types'
-
-const StyledPageWrapper = styled.div`
-  margin: 0 auto;
-
-  a {
-    color: ${colors.brand};
-  }
-
-  h1 {
-    font-size: 2rem;
-    text-rendering: optimizeLegibility;
-    letter-spacing: 2px;
-  }
-
-  h2 {
-    font-size: 1.45rem;
-  }
-
-  p {
-    padding: 0;
-  }
-
-  h1,
-  h2,
-  img,
-  p {
-    margin: 0 0 1.45rem;
-  }
-
-  ul {
-    list-style-type: disc;
-  }
-
-  @media only screen and (max-width: 480px) {
-    font-size: 18px;
-  }
-`
 
 interface PostTemplateProps {
   data: Query
@@ -50,16 +10,9 @@ interface PostTemplateProps {
 const PostTemplate: React.FunctionComponent<PostTemplateProps> = ({ data }) => {
   const frontmatter =
     (data && data.markdownRemark && data.markdownRemark.frontmatter) || {}
+  const title = frontmatter.title || ''
   const html = (data && data.markdownRemark && data.markdownRemark.html) || ''
-  return (
-    <Layout>
-      <StyledPageWrapper>
-        <Helmet title={`${frontmatter.title}`} />
-        <h1>{frontmatter.title}</h1>
-        <div dangerouslySetInnerHTML={{ __html: html }} />
-      </StyledPageWrapper>
-    </Layout>
-  )
+  return <BlogPost html={html} title={title} />
 }
 
 export default PostTemplate
