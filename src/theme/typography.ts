a3f058370c771da8e602d25977e071438cfb3868
Karol Partyka <k.partyka87@gmail.com>
2019-02-18 10:57:41 +0100

    style: blog content size
    Updating alphabetical order in typography declarations

--- src/theme/typography.ts
+++ src/theme/typography.ts
@@ -1,8 +1,8 @@
 export const typography = {
   basicFontFamily: `'Montserrat', sans-serif`,
+  blogContentSize: '18px',
   ctaSize: '15px',
   headingSize: '21px',
   paragraphSize: '15px',
   subtitleSize: '15px',
-  blogContentSize: '18px',
 }
