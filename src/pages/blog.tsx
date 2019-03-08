166969c9121a2f745da552e099be58e115c856dc
Piotr Kwiecinski <piokwiecinski@gmail.com>
2019-03-08 13:48:54 +0000

    style: format code

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -2,32 +2,35 @@ import * as React from 'react'
 import { StaticQuery, graphql } from 'gatsby'
 import BlogTemplate from '../templates/BlogTemplate'
 
-
 const BlogPage = () => (
-    <StaticQuery query={
-        graphql`
-            query {
-            allMarkdownRemark(filter: {fileAbsolutePath: {regex: "//blog//"}}, limit: 1000) {
-                edges {
-                node {
-                    id
-                    fields {
-                    slug
-                    }
-                    frontmatter {
-                    title
-                    date
-                    tags
-                    backgroundImage {
-                        publicURL
-                    }
-                    }
+  <StaticQuery
+    query={graphql`
+      query {
+        allMarkdownRemark(
+          filter: { fileAbsolutePath: { regex: "//blog//" } }
+          limit: 1000
+        ) {
+          edges {
+            node {
+              id
+              fields {
+                slug
+              }
+              frontmatter {
+                title
+                date
+                tags
+                backgroundImage {
+                  publicURL
                 }
-                }
+              }
             }
-            }`
-    }
-    render={data => ( <BlogTemplate data={data}/> )} />
+          }
+        }
+      }
+    `}
+    render={data => <BlogTemplate data={data} />}
+  />
 )
 
 export default BlogPage
